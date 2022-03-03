2a3599299b9edfb4bbb0f99d509761f24b97ead4
juanguz1999 <juanguz1999@outlook.com>
2022-03-03 11:00:43 -0500

    Asocialo con prueba

--- IndexFinal.php
+++ IndexFinal.php
@@ -7,9 +7,16 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
     <head>
         <meta charset="UTF-8">
         <meta name="google-signin-client_id" content="46479244231-85meh2sc6ibs1aumqv4hm9bf7sdirupl.apps.googleusercontent.com">
+        <link href="css/Nbar.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <title></title>
     </head>
     <body>
+        <nav class="Nbarra" role="navigation">
+            <div>
+                <img src="img/descarga.png" alt="" height="80 "width="100" />
+            </div>
+        </nav>
     <center><div class="container">
             <div class="row">
                 <div class="col">
@@ -20,21 +27,24 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
                 <h1>Iniciar Sesion</h1>
             </div>
         </div>
-    </center>
 
-    <!-- Boton Google-->
 
-    <center><div class="row">
+        <!-- Boton Google-->
+
+        <div class="row">
             <div class="col">
                 <div id="my-signin2"></div>
                 <script src="js/gg.js" type="text/javascript"></script>
             </div>
         </div>
+
+
+        <footer class="container">
+            <p class="float-end"><a href="#">Back to top</a></p>
+            <p>&copy; 2017–2021 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        </footer>
     </center>
 
-    <?php
-    // put your code here
-    ?>
     <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
 </body>
 </html>
