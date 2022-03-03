adc7163017f66130281f45d4998cbdb044dc984e
Preytax <100790085+Preytax@users.noreply.github.com>
2022-03-03 11:15:06 -0500

    Propuestafuncional

---
+++ PropuestaFuncional/IniciarSesion.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://apis.google.com/js/platform.js" async defer></script>
+  <meta name="google-signin-client_id" content="30639041895-0l8at7s382j7gkv9ka4th2vp8tjuk2tl.apps.googleusercontent.com">
+  <script src="Script.js" type="text/javascript"></script>
+  <title>Iniciar seccion</title>
+</head>
+<body>
+  <div id="my-signin2"></div>
+  
+
+  <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
+</body>
+</html>
